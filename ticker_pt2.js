d095ac13330f86dc8cc219f249efcdb1c0f47337
JusTuft <Justin.Projansky@tufts.edu>
2025-12-08 00:18:48 -0500

    Update ticker_pt2.js

--- ticker_pt2.js
+++ ticker_pt2.js
@@ -4,105 +4,110 @@ const MongoClient = require('mongodb').MongoClient;
 const MongoURL = "mongodb+srv://dbUser:dbUser@ticking.swchu4g.mongodb.net/?appName=Ticking";
 const PORT = process.env.PORT || 3000;
 
-client = new MongoClient(MongoURL,{ useUnifiedTopology: true });
-async function doit() {
-    try {
-        await client.connect();
-        
+client = new MongoClient(MongoURL);
+MongoClient.connect(MongoURL)
+    .then(client => {
         var dbo = client.db("Stock");
         var coll = dbo.collection('PublicCompanies');
+    })
+    .catch(err => {
+        console.log("Database error: " + err);
+        process.exit(1);
+    });
 
-        http.createServer(function (req, res) {
-            // Set up server and get url info
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            let theURL = url.parse(req.url, true);
-            let pathname = theURL.pathname;
-            let query = theURL.query;
+http.createServer(async function (req, res) {
+    // Set up server and get url info
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    let theURL = url.parse(req.url, true);
+    let pathname = theURL.pathname;
+    let query = theURL.query;
 
-            if (pathname.startsWith("/process")) {
-                // Get info from Mongo
-                var userInput = query.input;
-                var type = query.type;
+    if (pathname == "/") {
+        // Make a form for the user to input information
+        res.write(
+            `<!doctype html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <title>Stock Search</title>
+            </head>
+            <body>
+                <h1>Stock Form</h1>
+                <form method="GET" action="/process">
+                    <label>Are you inputting a stock ticker or company name?</label><br/>
+                        <label>
+                            <input type="radio" id="ticker" name="type" value="ticker" checked> Stock Ticker
+                        </label><br/>
+                        <label>
+                            <input type="radio" id="name" name="type" value="name"> Company Name
+                        </label><br/><br/>
 
-                var MongoQuery;
-                if (type == "ticker") {
-                    MongoQuery = { ticker: userInput.toUpperCase() };
-                } else {
-                    MongoQuery = { name: userInput };
+                        <label>
+                            Input Stock Ticker or Company Name Here: <input type="text" id="input" name="input" required>
+                        </label><br/><br/>
+
+                    <button type="submit">View Results</button>
+                </form>
+            </body>
+            </html>`
+        );
+        res.end();
+    } else if (pathname == "/process") {
+        // Get info from Mongo
+        var userInput = query.input;
+        var type = query.type;
+
+        var MongoQuery;
+        if (type == "ticker") {
+            MongoQuery = { ticker: userInput.toUpperCase() };
+        } else {
+            MongoQuery = { name: userInput };
+        }
+
+        // Display results of info from Mongo
+        coll.find(MongoQuery).toArray()
+            .then(results => {
+                if (err) {
+                    res.end("Database error: " + err);
+                    return;
                 }
-
-                // Display results of info from Mongo
-                let results = coll.find(MongoQuery).toArray(function(err, results) {
-                    if (err) {
-                        res.end("Database error: " + err);
-                        return;
-                    }
-                    res.write(`
-                        <!doctype html>
-                        <html lang="en">
-                        <head>
-                            <meta charset="UTF-8">
-                            <title>Stock Search</title>
-                        </head>
-                        <body>
-                            <h1>Search Results</h1>
-                            <h2>User Inputs:</h2>
-                            <p>Inputted Query: ${userInput}</p><br/>
-                            <p>Type of Search: ${type}</p><br/><br/>
-                    `);
-                    if (results.length > 0 && userInput != "") {
-                    res.write("<p>We found results for your query. Wow!</p><br/><br/>");
-                    res.write("<ul>");
-                        results.forEach(thingie => {
-                            res.write(`<li>${thingie.name}, ${thingie.ticker}, ${thingie.price}</li>`)
-                        })
-                        res.write("</ul>");
-                    } else {
-                        res.write("<p>We found no results for your query. Sorry!</p>");
-                    }
-                    res.write(`
-                        </body>
-                        </html>
-                    `);
-                    res.end();
-                });
-            } else {
-                // Make a form for the user to input information
-                res.write(
-                    `<!doctype html>
+                res.write(`
+                    <!doctype html>
                     <html lang="en">
                     <head>
                         <meta charset="UTF-8">
                         <title>Stock Search</title>
                     </head>
                     <body>
-                        <h1>Stock Form</h1>
-                        <form method="GET" action="/process">
-                            <label>Are you inputting a stock ticker or company name?</label><br/>
-                                <label>
-                                    <input type="radio" id="ticker" name="type" value="ticker" checked> Stock Ticker
-                                </label><br/>
-                                <label>
-                                    <input type="radio" id="name" name="type" value="name"> Company Name
-                                </label><br/><br/>
-
-                                <label>
-                                    Input Stock Ticker or Company Name Here: <input type="text" id="input" name="input" required>
-                                </label><br/><br/>
-
-                            <button type="submit">View Results</button>
-                        </form>
+                        <h1>Search Results</h1>
+                        <h2>User Inputs:</h2>
+                        <p>Inputted Query: ${userInput}</p><br/>
+                        <p>Type of Search: ${type}</p><br/><br/>
+                `);
+                if (results.length > 0 && userInput != "") {
+                res.write("<p>We found results for your query. Wow!</p><br/><br/>");
+                res.write("<ul>");
+                    results.forEach(thingie => {
+                        res.write(`<li>${thingie.name}, ${thingie.ticker}, ${thingie.price}</li>`)
+                    })
+                    res.write("</ul>");
+                } else {
+                    res.write("<p>We found no results for your query. Sorry!</p>");
+                }
+                res.write(`
                     </body>
-                    </html>`
-                )
-            }
-        }).listen(PORT, () => {
-            console.log("Server running on port " + PORT);
-        });
+                    </html>
+                `);
+                res.end();
+            })
+            .catch(err => {
+                console.log("Database error: " + err);
+                process.exit(1);
+            });
+    } else {
+        res.write("<p>I don't know how you got here. Go back!</p>");
+        res.end();
     }
-    catch(err) {
-        console.log("Database error: " + err);
-    }
-} //end doit
-
-doit();
+}).listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
