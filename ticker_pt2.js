fc175162109668851afb735bee9b3074635f615e
JusTuft <Justin.Projansky@tufts.edu>
2025-12-07 23:27:31 -0500

    Update ticker_pt2.js

--- ticker_pt2.js
+++ ticker_pt2.js
@@ -19,7 +19,54 @@ async function doit() {
             let pathname = theURL.pathname;
             let query = theURL.query;
 
-            if (pathname != "/process") {
+            if (pathname.startsWith("/process")) {
+                // Get info from Mongo
+                var userInput = query.input;
+                var type = query.type;
+
+                var MongoQuery;
+                if (type == "ticker") {
+                    MongoQuery = { ticker: userInput.toUpperCase() };
+                } else {
+                    MongoQuery = { name: userInput };
+                }
+
+                // Display results of info from Mongo
+                let results = coll.find(MongoQuery).toArray(function(err, results) {
+                    if (err) {
+                        res.end("Database error: " + err);
+                        return;
+                    }
+                    res.write(`
+                        <!doctype html>
+                        <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <title>Stock Search</title>
+                        </head>
+                        <body>
+                            <h1>Search Results</h1>
+                            <h2>User Inputs:</h2>
+                            <p>Inputted Query: ${userInput}</p><br/>
+                            <p>Type of Search: ${type}</p><br/><br/>
+                    `);
+                    if (results.length > 0 && userInput != "") {
+                    res.write("<p>We found results for your query. Wow!</p><br/><br/>");
+                    res.write("<ul>");
+                        results.forEach(thingie => {
+                            res.write(`<li>${thingie.name}, ${thingie.ticker}, ${thingie.price}</li>`)
+                        })
+                        res.write("</ul>");
+                    } else {
+                        res.write("<p>We found no results for your query. Sorry!</p>");
+                    }
+                    res.write(`
+                        </body>
+                        </html>
+                    `);
+                    res.end();
+                });
+            } else {
                 // Make a form for the user to input information
                 res.write(
                     `<!doctype html>
@@ -48,53 +95,6 @@ async function doit() {
                     </body>
                     </html>`
                 )
-            } else {
-                // Get info from Mongo
-                var userInput = query.input;
-                var type = query.type;
-
-                var MongoQuery;
-                if (type == "ticker") {
-                    MongoQuery = { ticker: userInput.toUpperCase() };
-                } else {
-                    MongoQuery = { name: userInput };
-                }
-
-                // Display results of info from Mongo
-                results = coll.find(MongoQuery).toArray(function(err, results) {
-                    if (err) {
-                        res.end("Database error: " + err);
-                        return;
-                    }
-                    res.write(`
-                        <!doctype html>
-                        <html lang="en">
-                        <head>
-                            <meta charset="UTF-8">
-                            <title>Stock Search</title>
-                        </head>
-                        <body>
-                            <h1>Search Results</h1>
-                            <h2>User Inputs:</h2>
-                            <p>Inputted Query: ${userInput}</p><br/>
-                            <p>Type of Search: ${type}</p><br/><br/>
-                    `);
-                    if (results.length > 0) {
-                    res.write("<p>We found results for your query. Wow!</p><br/><br/>");
-                    res.write("<ul>");
-                        results.forEach(thingie => {
-                            res.write(`<li>${thingie.name}, ${thingie.ticker}, ${thingie.price}</li>`)
-                        })
-                        res.write("</ul>");
-                    } else {
-                        res.write("<p>We found no results for your query. Sorry!</p>");
-                    }
-                    res.write(`
-                        </body>
-                        </html>
-                    `);
-                    res.end();
-                });
             }
         }).listen(PORT, () => {
             console.log("Server running on port " + PORT);
